b6d581a7cac8ff0839a274ca040640926085bd13
feder <federicaelia24@gmail.com>
2022-11-09 15:20:13 +0100

    avviata prima milestone

---
+++ Evento.cs
@@ -0,0 +1,80 @@
+﻿
+
+
+
+
+//classe evento
+public class Evento {
+    
+    //attributi
+    private string _titolo;
+    private DateOnly _data;
+
+    //properties
+    public string Titolo {
+        get {
+            return _titolo;
+        }
+        set {
+            if (_titolo != "")
+            {
+                _titolo = value;
+            }
+        } 
+    }
+    public DateOnly Data
+    {
+        get
+        {
+            return _data;
+        }
+        set
+        {
+            if (_data >= DateOnly.FromDateTime(DateTime.Now))
+            {
+                _data = value;
+            }
+        }
+    }
+    public int PostiMax { get; }
+    public int PostiPrenotati { get; private set; }
+
+    //costruttore
+    public Evento(string titolo, DateOnly data, int postiMax)
+    {
+        Titolo = titolo;
+        Data = data;
+        if (postiMax > 0)
+        {
+            PostiMax = postiMax;
+        }
+        PostiPrenotati = 0;
+    }
+
+    //metodi
+    public int PrenotaPosti(int inputPrenotazioni)
+    {
+        //se posti disponibili sono <= dei posti tot ok
+        //se data non è passata
+        if ((PostiMax - PostiPrenotati) >= inputPrenotazioni )
+        {
+            return PostiPrenotati += inputPrenotazioni;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int DisdiciPosti()
+    {
+        //metodo che si occuperà di gestire i posti cancellati ad un evento
+        return 0;
+    }
+
+    public override string ToString()
+    {
+        //metodo che si occuperà di ritornare una stringa dalla data dell'evento
+        return "data - titolo";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateOnly.FromDateTime(DateTime.Now));
+
+Console.WriteLine("inserisci nome evento");
+string nomeEvento = Console.ReadLine();
+
+Console.WriteLine("inserisci data evento (gg/mm/yyyy)");
+DateOnly dataEvento = DateOnly.Parse((Console.ReadLine()));
+
+Console.WriteLine("inserisci numero posti max evento");
+int postiMaxEvento = Convert.ToInt32(Console.ReadLine());
+
+Evento evento1 = new Evento (nomeEvento, dataEvento, postiMaxEvento);
+
+Console.WriteLine("inserisci numero posti da prenotare");
+int postiDaPrenotare = Convert.ToInt32(Console.ReadLine());
+evento1.PrenotaPosti(postiDaPrenotare);
+Console.WriteLine("posti prenotati: {0}", evento1.PostiPrenotati);
+
+//creare classe eccezioni per il programma
+public class gestoreEventiEccezioni
+{
+
+}
